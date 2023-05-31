0db808a2cff4a4372223606d2116d19551560d0e
Emily Curry <emily@curry.sh>
2023-05-31 13:18:24 -0500

    feat: add featured event series

--- packages/website/src/map-tournament-to-series.ts
+++ packages/website/src/map-tournament-to-series.ts
@@ -3,9 +3,14 @@ import * as z from "zod";
 
 export const mapTournamentToSeries = (
   tournament: z.TypeOf<typeof GGTournamentSchema>
-): string | undefined => {
+): string[] => {
+  const result = new Set<string>();
   if (tournament.name.toLocaleLowerCase().includes("magic pixel weekly"))
-    return "Magic Pixel Weekly";
+    result.add("Magic Pixel Weekly");
 
-  return undefined;
+  if (tournament.name.toLocaleLowerCase().includes("monthly"))
+    result.add("Featured");
+  if (tournament.slug === "kansas-k-o") result.add("Featured");
+
+  return [...result];
 };
