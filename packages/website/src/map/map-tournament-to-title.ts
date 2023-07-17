fd9212b1791ad15db92a5272f15bbe39c43d9fdf
Emily Curry <emily@curry.sh>
2023-07-17 17:51:40 -0500

    chore: update links

--- packages/website/src/map/map-tournament-to-title.ts
+++ packages/website/src/map/map-tournament-to-title.ts
@@ -1,12 +1,8 @@
 import { GGTournamentSchema } from "@emily-curry/fgc-sync-common";
 import * as z from "zod";
-import { mapTournamentToSeries } from "./map-tournament-to-series";
 
 export const mapTournamentToTitle = (
   tournament: z.TypeOf<typeof GGTournamentSchema>
 ): string => {
-  const series = mapTournamentToSeries(tournament);
-  if (series.includes("Magic Pixel Weekly")) return "Magic Pixel Weekly";
-
   return tournament.name;
 };
