932991590b9960338c625ebb4e073a6e7a172388
Emily Curry <emily@curry.sh>
2023-10-14 10:55:58 -0500

    feat: add runback to series

--- packages/website/src/map/map-tournament-to-series.ts
+++ packages/website/src/map/map-tournament-to-series.ts
@@ -8,13 +8,10 @@ export const mapTournamentToSeries = (
   if (tournament.name.toLocaleLowerCase().includes("magic pixel weekly"))
     result.add("Magic Pixel Weekly");
 
-  if (tournament.name.toLocaleLowerCase().includes("sunday fight fest"))
-    result.add("Sunday Fight Fest");
+  if (tournament.name.toLocaleLowerCase().includes("magic pixel runback"))
+    result.add("Magic Pixel Runback");
 
   if (tournament.name.toLocaleLowerCase().includes("monthly"))
-    result.add("Featured");
-  if (tournament.slug === "kansas-k-o") result.add("Featured");
-  if (tournament.slug.toLocaleLowerCase().includes("dsm-snapback"))
     result.add("Featured");
 
   return [...result];
